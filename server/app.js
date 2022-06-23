5585c50d764f73ae3de9c3784b341d791a999ba9
Prabhat Mani Srivastava <prabhatmani933@gmail.com>
2022-06-24 01:09:14 +0530

    Set MVC pattern.

--- server/app.js
+++ server/app.js
@@ -1,6 +1,15 @@
 const express = require('express')
+const planetsRouter = require('./src/routes/planets/planets.router')
+const cors = require('cors')
 
 const app = express()
+
+app.use(
+  cors({
+    origin: 'http://localhost:3000',
+  })
+)
 app.use(express.json())
+app.use(planetsRouter)
 
 module.exports = app
--- server/src/server.js
+++ server/src/server.js
@@ -1,5 +1,5 @@
 const http = require('http')
-const app = require('./app')
+const app = require('../app')
 
 const PORT = process.env.PORT || 8000
 
