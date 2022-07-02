aaa756910d0ff70192e9023463d6691f25e89907
Prabhat Mani Srivastava <prabhatmani933@gmail.com>
2022-07-03 02:32:11 +0530

    created build of client(frontend).

--- server/app.js
+++ server/app.js
@@ -1,17 +1,22 @@
 const express = require('express')
 const planetsRouter = require('./src/routes/planets/planets.router')
 const cors = require('cors')
-
-const PORT = process.env.PORT || 5000
+const path = require('path')
 
 const app = express()
 
 app.use(
   cors({
-    origin: PORT, //|| 'http://localhost:3000',
+    origin: 'http://localhost:3000',
   })
 )
 app.use(express.json())
+app.use(express.static(path.join(__dirname, 'public'))) //for serving static files on server.
+
 app.use(planetsRouter)
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'))
+})
 
 module.exports = app
