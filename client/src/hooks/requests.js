e8edfc43b4c7a007363d56ed6a67ca03481e2ddc
Prabhat Mani Srivastava <prabhatmani933@gmail.com>
2022-07-04 00:35:47 +0530

    Created launches model for upcoming and history sections.

--- client/src/hooks/requests.js
+++ client/src/hooks/requests.js
@@ -1,3 +1,5 @@
+import { response } from '../../../server/app'
+
 const API_URL = 'http://localhost:8000'
 
 // Load planets and return as JSON.
@@ -6,9 +8,14 @@ async function httpGetPlanets() {
   return await response.json()
 }
 
+// TODO: Once API is ready.
+// Load launches, sort by flight number, and return as JSON.
 async function httpGetLaunches() {
-  // TODO: Once API is ready.
-  // Load launches, sort by flight number, and return as JSON.
+  const response = await fetch(`${API_URL}/launches`)
+  const fetchedLaunches = await response.json()
+  return fetchedLaunches.sort((a, b) => {
+    return (a.flightNumber = b.flightNumber)
+  })
 }
 
 async function httpSubmitLaunch(launch) {
--- server/app.js
+++ server/app.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const planetsRouter = require('./src/routes/planets/planets.router')
+const launchesRouter = require('./src/routes/launches/launches.router')
 const cors = require('cors')
 const path = require('path')
+const morgan = require('morgan')
 
 const app = express()
 
@@ -10,13 +12,18 @@ app.use(
     origin: 'http://localhost:3000',
   })
 )
+app.use(morgan('combined'))
+
 app.use(express.json())
 app.use(express.static(path.join(__dirname, 'public'))) //for serving static files on server.
 
 app.use(planetsRouter)
+app.use(launchesRouter)
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'))
 })
+
+app.get('/', (req, res) => {})
 
 module.exports = app
